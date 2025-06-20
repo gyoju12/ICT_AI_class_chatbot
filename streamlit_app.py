0c19aee678b48dc5a777a3cd351b8ae0996917f9
대용량봉지 <gyoju12@gmail.com>
2025-06-20 16:28:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 MIMIC's Chatbot 💬")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "이것은 OpenAI의 GPT-4o-mini 모델을 사용하여 응답을 생성하는 간단한 챗봇입니다."
+    "이 앱을 사용하려면 OpenAI API 키를 제공해야 하며, 키는 [여기](https://platform.openai.com/account/api-keys)에서 얻을 수 있습니다."
+    "또한, [튜토리얼](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)을 따라하며 이 앱을 단계별로 만드는 방법을 배울 수도 있습니다."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
@@ -41,7 +41,7 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4o-mini",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
